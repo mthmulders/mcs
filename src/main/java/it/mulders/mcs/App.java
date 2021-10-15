fe629c8be43aaa557b72d3c18eb01255c7c5b231
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-10-15 20:53:50 +0200

    Add search command CLI

--- src/main/java/it/mulders/mcs/App.java
+++ src/main/java/it/mulders/mcs/App.java
@@ -1,12 +1,16 @@
 package it.mulders.mcs;
 
+import it.mulders.mcs.search.SearchCommand;
 import picocli.CommandLine;
 
+@CommandLine.Command(
+        name = "mcs",
+        subcommands = { Test.class, SearchCommand.class },
+        usageHelpAutoWidth = true
+)
 public class App {
     public static void main(final String... args) {
-        var program = new CommandLine(
-                new Test()
-        );
+        var program = new CommandLine(new App());
         var result = program.execute(args);
         System.exit(result);
     }
