f123269f4cac5e2a344e4a269a9300664f4a4316
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-11-17 22:19:02 +0100

    Refactor into smaller methods

--- src/main/java/it/mulders/mcs/search/SearchCommandHandler.java
+++ src/main/java/it/mulders/mcs/search/SearchCommandHandler.java
@@ -7,31 +7,39 @@ public class SearchCommandHandler {
     public void search(final String query) {
         System.out.printf("Searching for %s...%n", query);
 
-        if (query.contains(":")) {
-            var parts = query.split(":");
-            if (parts.length < 2) {
-                var msg = """
+        if (isCoordinateSearch(query)) {
+            performCoordinateSearch(query);
+        } else {
+            performWildcardSearch(query);
+        }
+    }
+
+    private void performWildcardSearch(final String query) {
+        searchClient.wildcardSearch(query)
+                .map(SearchResponse::response)
+                .ifPresent(this::printResponse);
+    }
+
+    private void performCoordinateSearch(final String query) {
+        var parts = query.split(":");
+        if (parts.length < 2) {
+            var msg = """
                         Searching a particular artifact requires at least groupId:artifactId and optionally :version
                         """;
-                throw new IllegalArgumentException(msg);
-            }
+            throw new IllegalArgumentException(msg);
+        }
 
-            var groupId = parts[0];
-            var artifactId = parts[1];
-            var hasVersion = parts.length == 3;
-            if (hasVersion) {
-                var version = parts[2];
+        var groupId = parts[0];
+        var artifactId = parts[1];
+        var hasVersion = parts.length == 3;
+        if (hasVersion) {
+            var version = parts[2];
 
-                searchClient.singularSearch(groupId, artifactId, version)
-                        .map(SearchResponse::response)
-                        .ifPresent(this::printResponse);
-            } else {
-                searchClient.singularSearch(groupId, artifactId)
-                        .map(SearchResponse::response)
-                        .ifPresent(this::printResponse);
-            }
+            searchClient.singularSearch(groupId, artifactId, version)
+                    .map(SearchResponse::response)
+                    .ifPresent(this::printResponse);
         } else {
-            searchClient.wildcardSearch(query)
+            searchClient.singularSearch(groupId, artifactId)
                     .map(SearchResponse::response)
                     .ifPresent(this::printResponse);
         }
@@ -39,5 +47,9 @@ public class SearchCommandHandler {
 
     private void printResponse(final SearchResponse.Response response) {
         outputPrinter.print(response, System.out);
+    }
+
+    private boolean isCoordinateSearch(final String query) {
+        return query.contains(":");
     }
 }
