999d1e1bdc4b7377ab911f1e8d43ffd381de5568
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-11-17 21:14:21 +0100

    test: Add test for response on coordinate search without version

--- src/test/java/it/mulders/mcs/common/SearchResponseBodyHandlerTest.java
+++ src/test/java/it/mulders/mcs/common/SearchResponseBodyHandlerTest.java
@@ -9,76 +9,108 @@ import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class SearchResponseBodyHandlerTest implements WithAssertions {
-    @Nested
-    class WildcardSearchResponse {
-        @Test
-        void parse_response() {
-            // Arrange
-            var input = getClass().getResourceAsStream("/wildcard-search-response.json");
+    /**
+     * This is the response we get when the user uses the "wildcard" search -
+     * not looking for an exact coordinate, but looking for something like "plexus-utils".
+     */
+    @Test
+    void parse_wildcard_search_response() {
+        // Arrange
+        var input = getClass().getResourceAsStream("/wildcard-search-response.json");
 
-            // Act
-            var response = SearchResponseBodyHandler.toSupplierOfSearchResponse(input).get();
+        // Act
+        var response = SearchResponseBodyHandler.toSupplierOfSearchResponse(input).get();
 
-            // Assert
-            assertThat(response.response()).isNotNull();
-            assertThat(response.response().numFound()).isEqualTo(2);
-            assertThat(response.response().start()).isEqualTo(0);
-            assertThat(response.response().docs()).hasSize(2);
+        // Assert
+        assertThat(response.response()).isNotNull();
+        assertThat(response.response().numFound()).isEqualTo(2);
+        assertThat(response.response().start()).isEqualTo(0);
+        assertThat(response.response().docs()).hasSize(2);
 
-            assertThat(response.response().docs()).contains(
-                    new SearchResponse.Response.Doc(
-                            "org.codehaus.plexus:plexus-utils",
-                            "org.codehaus.plexus",
-                            "plexus-utils",
-                            null,
-                            "3.4.1",
-                            "jar",
-                    1630022910000L
-                    )
-            );
-
-            assertThat(response.response().docs()).contains(
-                    new SearchResponse.Response.Doc(
-                        "plexus:plexus-utils",
-                        "plexus",
+        assertThat(response.response().docs()).contains(
+                new SearchResponse.Response.Doc(
+                        "org.codehaus.plexus:plexus-utils",
+                        "org.codehaus.plexus",
                         "plexus-utils",
                         null,
-                        "1.0.3",
+                        "3.4.1",
                         "jar",
-                        1131487245000L
-                    )
-            );
-        }
+                1630022910000L
+                )
+        );
+
+        assertThat(response.response().docs()).contains(
+                new SearchResponse.Response.Doc(
+                    "plexus:plexus-utils",
+                    "plexus",
+                    "plexus-utils",
+                    null,
+                    "1.0.3",
+                    "jar",
+                    1131487245000L
+                )
+        );
     }
 
-    @Nested
-    class SingleSearchResponse {
-        @Test
-        void parse_response() {
-            // Arrange
-            var input = getClass().getResourceAsStream("/specific-search-response.json");
+    /**
+     * This is the response we get when the user searches for a coordinate including a version -
+     * e.g. "org.codehaus.plexus:plexus-utils:3.4.1".
+     */
+    @Test
+    void parse_coordinates_with_version_search_response() {
+        // Arrange
+        var input = getClass().getResourceAsStream("/group-artifact-version-search.json");
 
-            // Act
-            var response = SearchResponseBodyHandler.toSupplierOfSearchResponse(input).get();
+        // Act
+        var response = SearchResponseBodyHandler.toSupplierOfSearchResponse(input).get();
 
-            // Assert
-            assertThat(response.response()).isNotNull();
-            assertThat(response.response().numFound()).isEqualTo(1);
-            assertThat(response.response().start()).isEqualTo(0);
-            assertThat(response.response().docs()).hasSize(1);
+        // Assert
+        assertThat(response.response()).isNotNull();
+        assertThat(response.response().numFound()).isEqualTo(1);
+        assertThat(response.response().start()).isEqualTo(0);
+        assertThat(response.response().docs()).hasSize(1);
 
-            assertThat(response.response().docs()).contains(
-                    new SearchResponse.Response.Doc(
-                            "org.codehaus.plexus:plexus-utils:3.4.1",
-                            "org.codehaus.plexus",
-                            "plexus-utils",
-                            "3.4.1",
-                            null,
-                            "jar",
-                            1630022910000L
-                    )
-            );
-        }
+        assertThat(response.response().docs()).contains(
+                new SearchResponse.Response.Doc(
+                        "org.codehaus.plexus:plexus-utils:3.4.1",
+                        "org.codehaus.plexus",
+                        "plexus-utils",
+                        "3.4.1",
+                        null,
+                        "jar",
+                        1630022910000L
+                )
+        );
     }
 
+    /**
+     * This is the response we get when the user searches for a coordinate without a version -
+     * e.g. "org.codehaus.plexus:plexus-utils".
+     */
+    @Test
+    void parse_coordinates_without_version_search_response() {
+        // Arrange
+        var input = getClass().getResourceAsStream("/group-artifact-search.json");
+
+        // Act
+        var response = SearchResponseBodyHandler.toSupplierOfSearchResponse(input).get();
+
+        // Assert
+        assertThat(response.response()).isNotNull();
+        assertThat(response.response().numFound()).isEqualTo(1);
+        assertThat(response.response().start()).isEqualTo(0);
+        assertThat(response.response().docs()).hasSize(1);
+
+        assertThat(response.response().docs()).contains(
+                new SearchResponse.Response.Doc(
+                        "org.codehaus.plexus:plexus-utils",
+                        "org.codehaus.plexus",
+                        "plexus-utils",
+                        null,
+                        "3.4.1",
+                        "jar",
+                        1630022910000L
+                )
+        );
+    }
 }
