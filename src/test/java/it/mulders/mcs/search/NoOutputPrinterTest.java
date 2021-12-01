aac99fb261e252e552852b5329049bc59b1fabd8
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-12-01 21:48:09 +0100

    test: Cover non-tested code

---
+++ src/test/java/it/mulders/mcs/search/NoOutputPrinterTest.java
@@ -0,0 +1,33 @@
+package it.mulders.mcs.search;
+
+import org.apache.commons.io.output.NullOutputStream;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+class NoOutputPrinterTest implements WithAssertions {
+    private final OutputPrinter printer = new NoOutputPrinter();
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @Test
+    void should_fail_with_non_empty_result() {
+        assertThatThrownBy(() -> printer.print(responseWithResult(1), new PrintStream(outputStream)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> printer.print(responseWithResult(2), new PrintStream(outputStream)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void should_print_message_with_empty_result() {
+        printer.print(responseWithResult(0), new PrintStream(outputStream));
+        assertThat(outputStream.toString(StandardCharsets.UTF_8)).contains("No results found");
+    }
+
+    private SearchResponse.Response responseWithResult(int count) {
+        return new SearchResponse.Response(count, 0, new SearchResponse.Response.Doc[0]);
+    }
+}
