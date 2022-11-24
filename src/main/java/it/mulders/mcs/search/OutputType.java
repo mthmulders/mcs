03970f54ea57a8bc16e18d6d1066614c8aa1cc11
Branislav Beno <branislav.beno@siemens.com>
2022-11-24 14:02:49 +0100

    Add OutputTypeTest

--- src/main/java/it/mulders/mcs/search/OutputType.java
+++ src/main/java/it/mulders/mcs/search/OutputType.java
@@ -1,7 +1,6 @@
 package it.mulders.mcs.search;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 public enum OutputType {
     MAVEN("maven"),
@@ -15,11 +14,16 @@ public enum OutputType {
     }
 
     public static OutputType parse(String text) {
-        return Optional.ofNullable(text)
-                .map(t -> Arrays.stream(values())
-                        .filter(type -> type.label.equals(text))
-                        .findFirst()
-                        .orElseThrow(() -> new IllegalArgumentException("Output format '%s' is not supported.".formatted(text))))
-                .orElse(Constants.OUTPUT_TYPE);
+        if (text == null) {
+            return Constants.OUTPUT_TYPE;
+        }
+        if (text.isBlank()) {
+            throw new IllegalArgumentException("Output format is empty.");
+        }
+
+        return Arrays.stream(values())
+                .filter(type -> type.label.equals(text))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Output format '%s' is not supported.".formatted(text)));
     }
 }
---
+++ src/test/java/it/mulders/mcs/search/OutputTypeTest.java
@@ -0,0 +1,37 @@
+package it.mulders.mcs.search;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OutputTypeTest {
+
+    @ParameterizedTest
+    @NullSource
+    void return_default_output_type_when_input_is_null(String parameter) {
+        assertThat(OutputType.parse(parameter)).isEqualTo(Constants.OUTPUT_TYPE);
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {" ", "nuget"})
+    void throw_exception_when_input_is_blank_or_unknown(String parameter) {
+        assertThrows(IllegalArgumentException.class, () -> OutputType.parse(parameter));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "maven,MAVEN",
+            "gradle,GRADLE",
+            "gradle-short,GRADLE_SHORT",
+    })
+    void return_expected_output_type_when_input_is_valid(String actual, OutputType expected) {
+        OutputType type = OutputType.parse(actual);
+        assertThat(type).isEqualTo(expected);
+    }
+}
