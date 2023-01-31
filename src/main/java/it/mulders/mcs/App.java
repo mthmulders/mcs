ba2dd5c8ece5fcb04cadda7aad8b06a52f68027e
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-01-31 21:42:38 +0100

    fix: Leverage PicoCLI error handling
    This hide stack traces, which are an
    implementation detail and should not be
    presented to the end user.

--- src/main/java/it/mulders/mcs/App.java
+++ src/main/java/it/mulders/mcs/App.java
@@ -3,8 +3,14 @@ package it.mulders.mcs;
 import it.mulders.mcs.cli.Cli;
 import it.mulders.mcs.cli.CommandClassFactory;
 import picocli.CommandLine;
+import picocli.CommandLine.IExecutionExceptionHandler;
 
 public class App {
+    private static final IExecutionExceptionHandler executionExceptionHandler = (ex, commandLine, parseResult) -> {
+        System.err.println(ex.getLocalizedMessage());
+        return -1;
+    };
+
     public static void main(final String... args) {
         System.exit(doMain(args));
     }
@@ -12,7 +18,8 @@ public class App {
     // Visible for testing
     static int doMain(final String... args) {
         var cli = new Cli();
-        var program = new CommandLine(cli, new CommandClassFactory(cli));
+        var program = new CommandLine(cli, new CommandClassFactory(cli))
+                .setExecutionExceptionHandler(executionExceptionHandler);
         return program.execute(args);
     }
 }
