7c8b5bc9c7f26865d6785e06051e73317c06af2c
Giovanni van der Schelde <gvdschelde@gmail.com>
2023-06-25 10:17:34 +0200

    feat: Display 'plugin' for maven-plugins
    Closes #236
    Fixes #232

--- src/test/java/it/mulders/mcs/search/printer/CoordinatePrinterTest.java
+++ src/test/java/it/mulders/mcs/search/printer/CoordinatePrinterTest.java
@@ -14,6 +14,17 @@ import java.util.stream.Stream;
 class CoordinatePrinterTest implements WithAssertions {
 
     private static final SearchQuery QUERY = SearchQuery.search("org.codehaus.plexus:plexus-utils").build();
+    private static final SearchResponse.Response PLUGIN_RESPONSE = new SearchResponse.Response(1, 0, new SearchResponse.Response.Doc[]{
+        new SearchResponse.Response.Doc(
+            "org.apache.maven.plugins:maven-jar-plugin:3.3.0",
+            "org.apache.maven.plugins",
+            "maven-jar-plugin",
+            "3.3.0",
+            null,
+            "maven-plugin",
+            1630022910000L
+        )
+    });
     private static final SearchResponse.Response RESPONSE =
             new SearchResponse.Response(1, 0, new SearchResponse.Response.Doc[]{
                     new SearchResponse.Response.Doc(
@@ -26,12 +37,19 @@ class CoordinatePrinterTest implements WithAssertions {
                             1630022910000L
                     )
             });
-    private static final String POM_XML_OUTPUT = """
+    private static final String POM_XML_DEPENDENCY_OUTPUT = """
             <dependency>
                 <groupId>org.codehaus.plexus</groupId>
                 <artifactId>plexus-utils</artifactId>
                 <version>3.4.1</version>
             </dependency>
+            """;
+    private static final String POM_XML_PLUGIN_OUTPUT = """
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-jar-plugin</artifactId>
+                <version>3.3.0</version>
+            </plugin>
             """;
     private static final String GRADLE_GROOVY_OUTPUT = "implementation group: 'org.codehaus.plexus', name: 'plexus-utils', version: '3.4.1'";
     private static final String GRADLE_GROOVY_SHORT_OUTPUT = "implementation 'org.codehaus.plexus:plexus-utils:3.4.1'";
@@ -54,22 +72,23 @@ class CoordinatePrinterTest implements WithAssertions {
 
     private static Stream<Arguments> coordinatePrinters() {
         return Stream.of(
-                Arguments.of(new PomXmlOutput(), POM_XML_OUTPUT),
-                Arguments.of(new GradleGroovyOutput(), GRADLE_GROOVY_OUTPUT),
-                Arguments.of(new GradleGroovyShortOutput(), GRADLE_GROOVY_SHORT_OUTPUT),
-                Arguments.of(new GradleKotlinOutput(), GRADLE_KOTLIN_OUTPUT),
-                Arguments.of(new SbtOutput(), SBT_OUTPUT),
-                Arguments.of(new IvyXmlOutput(), IVY_XML_OUTPUT),
-                Arguments.of(new GrapeOutput(), GRAPE_OUTPUT),
-                Arguments.of(new LeiningenOutput(), LEININGEN_OUTPUT),
-                Arguments.of(new BuildrOutput(), BUILDR_OUTPUT)
+                Arguments.of(new PomXmlOutput(), POM_XML_DEPENDENCY_OUTPUT, RESPONSE),
+                Arguments.of(new PomXmlOutput(), POM_XML_PLUGIN_OUTPUT, PLUGIN_RESPONSE),
+                Arguments.of(new GradleGroovyOutput(), GRADLE_GROOVY_OUTPUT, RESPONSE),
+                Arguments.of(new GradleGroovyShortOutput(), GRADLE_GROOVY_SHORT_OUTPUT, RESPONSE),
+                Arguments.of(new GradleKotlinOutput(), GRADLE_KOTLIN_OUTPUT, RESPONSE),
+                Arguments.of(new SbtOutput(), SBT_OUTPUT, RESPONSE),
+                Arguments.of(new IvyXmlOutput(), IVY_XML_OUTPUT, RESPONSE),
+                Arguments.of(new GrapeOutput(), GRAPE_OUTPUT, RESPONSE),
+                Arguments.of(new LeiningenOutput(), LEININGEN_OUTPUT, RESPONSE),
+                Arguments.of(new BuildrOutput(), BUILDR_OUTPUT, RESPONSE)
         );
     }
 
     @ParameterizedTest
     @MethodSource("coordinatePrinters")
-    void should_print_snippet(CoordinatePrinter printer, String expected) {
-        printer.print(QUERY, RESPONSE, new PrintStream(buffer));
+    void should_print_snippet(CoordinatePrinter printer, String expected, SearchResponse.Response response) {
+        printer.print(QUERY, response, new PrintStream(buffer));
         var xml = buffer.toString();
 
         assertThat(xml).isEqualToIgnoringWhitespace(expected);
