50ec031406a134b4e6a82913bac45d662cafd2e0
Usman Shaikh <ushaikh@sonatype.com>
2023-08-21 20:06:17 +0100

    print vuls

--- src/main/java/it/mulders/mcs/search/printer/CoordinatePrinter.java
+++ src/main/java/it/mulders/mcs/search/printer/CoordinatePrinter.java
@@ -2,8 +2,12 @@ package it.mulders.mcs.search.printer;
 
 import it.mulders.mcs.search.SearchQuery;
 import it.mulders.mcs.search.SearchResponse;
+import it.mulders.mcs.search.vulnerability.ComponentReportResponse;
+import it.mulders.mcs.search.vulnerability.ComponentReportResponse.ComponentReport.ComponentReportVulnerability;
 
 import java.io.PrintStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public sealed interface CoordinatePrinter extends OutputPrinter
         permits BuildrOutput, GradleGroovyOutput, GradleGroovyShortOutput, GradleKotlinOutput, GrapeOutput,
@@ -21,6 +25,7 @@ public sealed interface CoordinatePrinter extends OutputPrinter
         stream.println();
         stream.println(provideCoordinates(doc.g(), doc.a(), first(doc.v(), doc.latestVersion()), doc.p()));
         stream.println();
+        printVulnerabilities(doc.componentReport(), stream);
     }
 
     private String first(final String... values) {
@@ -30,5 +35,20 @@ public sealed interface CoordinatePrinter extends OutputPrinter
             }
         }
         return null;
+    }
+
+    private void printVulnerabilities(final ComponentReportResponse.ComponentReport componentReport,
+                                      final PrintStream stream) {
+        if (componentReport != null && componentReport.vulnerabilities().length > 0) {
+            var text = "Found %s vulnerabilities %s"
+                .formatted(
+                    componentReport.vulnerabilities().length,
+                    Stream.of(componentReport.vulnerabilities())
+                        .map(ComponentReportVulnerability::id)
+                        .collect(Collectors.joining(", ", "(", ")"))
+                );
+            stream.println(text);
+            stream.println("Reference: " + componentReport.reference());
+        }
     }
 }
--- src/main/java/it/mulders/mcs/search/printer/TabularOutputPrinter.java
+++ src/main/java/it/mulders/mcs/search/printer/TabularOutputPrinter.java
@@ -1,17 +1,20 @@
 package it.mulders.mcs.search.printer;
 
-import it.mulders.mcs.search.SearchQuery;
-import it.mulders.mcs.search.SearchResponse;
-import picocli.CommandLine;
-import picocli.CommandLine.Help;
-import picocli.CommandLine.Help.Ansi;
-import picocli.CommandLine.Help.Column.Overflow;
-
 import java.io.PrintStream;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import it.mulders.mcs.search.SearchQuery;
+import it.mulders.mcs.search.SearchResponse;
+import it.mulders.mcs.search.vulnerability.ComponentReportResponse.ComponentReport.ComponentReportVulnerability;
+import picocli.CommandLine;
+import picocli.CommandLine.Help;
+import picocli.CommandLine.Help.Ansi;
+import picocli.CommandLine.Help.Column.Overflow;
 
 public class TabularOutputPrinter implements OutputPrinter {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
@@ -38,11 +41,12 @@ public class TabularOutputPrinter implements OutputPrinter {
 
         var table = CommandLine.Help.TextTable.forColumns(colorScheme,
                 new CommandLine.Help.Column(maxKeyLength + SPACING, INDENT, Overflow.SPAN),
-                new CommandLine.Help.Column(30, INDENT, Overflow.WRAP)
+                new CommandLine.Help.Column(30, INDENT, Overflow.WRAP),
+                new CommandLine.Help.Column(300, INDENT, Overflow.SPAN)
         );
 
-        table.addRowValues("Coordinates", "Last updated");
-        table.addRowValues("===========", "============");
+        table.addRowValues("Coordinates", "Last updated", "Vulnerabilities");
+        table.addRowValues("===========", "============", "===============");
         Arrays.stream(response.docs()).forEach(doc -> printRow(table, doc));
 
         stream.println(table);
@@ -57,13 +61,29 @@ public class TabularOutputPrinter implements OutputPrinter {
     }
 
     private void printRow(final Help.TextTable table, final SearchResponse.Response.Doc doc) {
+       var vulnerabilities = "";
+       if (doc.componentReport() != null) {
+            var numVuls = doc.componentReport().vulnerabilities().length;
+            if (numVuls > 0) {
+              vulnerabilities += "Found " + numVuls + " vulnerabilities ";
+              //vulnerabilities += doc.componentReport().reference();
+              vulnerabilities += Stream.of(doc.componentReport().vulnerabilities())
+                  .map(ComponentReportVulnerability::id).collect(Collectors.joining(", ", "(", ")"));
+            }
+
+       }
+
         var lastUpdated = DATE_TIME_FORMATTER.format(
                 Instant.ofEpochMilli(doc.timestamp()).atZone(ZoneId.systemDefault())
         );
 
         var entry = displayEntry(doc);
 
-        table.addRowValues(entry, lastUpdated);
+        if (vulnerabilities != "") {
+            table.addRowValues(entry, lastUpdated, vulnerabilities);
+        } else {
+            table.addRowValues(entry, lastUpdated);
+        }
     }
 
     private String displayEntry(final SearchResponse.Response.Doc doc) {
