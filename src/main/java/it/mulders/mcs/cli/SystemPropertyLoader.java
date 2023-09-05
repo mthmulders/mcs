9a19855e02152b4e5263d37d6e7e682261cac8e3
Maarten Mulders <m.th.mulders@xs4all.nl>
2023-09-05 21:55:48 +0200

    feat: Load System properties from configuration file

---
+++ src/main/java/it/mulders/mcs/cli/SystemPropertyLoader.java
@@ -0,0 +1,72 @@
+package it.mulders.mcs.cli;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+/**
+ * Loads additional system properties from a predefined file on disk.
+ * <br />
+ * Copies those properties over the existing ones @{{@link System#getProperties()}} so the result
+ * is a drop-in replacement that could be passed for {@link System#setProperties(Properties)}.
+ * This class does not modify the System properties itself.
+ */
+public class SystemPropertyLoader {
+    private static final Path MCS_PROPERTIES_FILE = Paths.get(
+            System.getProperty("user.home"),
+            ".mcs",
+            "mcs.config"
+    );
+
+    private Properties properties;
+
+    public SystemPropertyLoader() {
+        this(MCS_PROPERTIES_FILE);
+    }
+
+    protected SystemPropertyLoader(final Path source) {
+        var input = new Properties();
+
+        if (Files.exists(source) && Files.isRegularFile(source)) {
+            try (var reader = Files.newBufferedReader(source)) {
+                input.load(reader);
+            } catch (IOException ioe) {
+                System.err.printf("Failed to load %s: %s%n", source, ioe.getLocalizedMessage());
+            }
+        }
+
+        var interpolatedProperties = interpolate(input);
+
+        this.properties = new Properties();
+        properties.putAll(System.getProperties());
+        properties.putAll(interpolatedProperties);
+    }
+
+    private static final Pattern INTERPOLATABLE_PROPERTY = Pattern.compile(".*(\\$\\{.*}).*");
+    private Properties interpolate(final Properties input) {
+        var result = new Properties();
+        input.forEach((key, value) -> {
+            var originalValue = value.toString();
+            var matcher = INTERPOLATABLE_PROPERTY.matcher(originalValue);
+            if (matcher.find()) {
+                var property = matcher.group(1);
+                var propertyName = property.substring(2, property.length() - 1);
+                var interimValue = input.getProperty(propertyName, System.getProperty(property));
+
+                var newValue = originalValue.replaceAll("\\$\\{" + propertyName + "}", interimValue);
+
+                result.put(key, newValue);
+            } else {
+                result.put(key, value);
+            }
+        });
+        return result;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+}
