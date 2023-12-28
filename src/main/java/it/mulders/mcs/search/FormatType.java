d8fb3b0be6b264e9f59860163cbff04c0eeba98f
Max Rydahl Andersen <max.andersen@gmail.com>
2023-12-28 23:43:33 +0100

    feat: add jbang and gav output formats

--- src/main/java/it/mulders/mcs/search/FormatType.java
+++ src/main/java/it/mulders/mcs/search/FormatType.java
@@ -1,17 +1,20 @@
 package it.mulders.mcs.search;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import it.mulders.mcs.search.printer.BuildrOutput;
 import it.mulders.mcs.search.printer.CoordinatePrinter;
+import it.mulders.mcs.search.printer.GavOutput;
 import it.mulders.mcs.search.printer.GradleGroovyOutput;
 import it.mulders.mcs.search.printer.GradleGroovyShortOutput;
 import it.mulders.mcs.search.printer.GradleKotlinOutput;
 import it.mulders.mcs.search.printer.GrapeOutput;
 import it.mulders.mcs.search.printer.IvyXmlOutput;
+import it.mulders.mcs.search.printer.JBangOutput;
 import it.mulders.mcs.search.printer.LeiningenOutput;
 import it.mulders.mcs.search.printer.PomXmlOutput;
 import it.mulders.mcs.search.printer.SbtOutput;
-
-import java.util.Arrays;
 
 public enum FormatType {
     MAVEN("maven", new PomXmlOutput()),
@@ -22,7 +25,9 @@ public enum FormatType {
     IVY("ivy", new IvyXmlOutput()),
     GRAPE("grape", new GrapeOutput()),
     LEININGEN("leiningen", new LeiningenOutput()),
-    BUILDR("buildr", new BuildrOutput());
+    BUILDR("buildr", new BuildrOutput()),
+    JBANG("jbang", new JBangOutput()),
+    GAV("gav", new GavOutput());
 
     private final String label;
     private final CoordinatePrinter printer;
@@ -36,18 +41,27 @@ public enum FormatType {
         return printer;
     }
 
+    static String commaSeparatedLabels() {
+        return Arrays.stream(values())
+                .map(type -> type.label)
+                .collect(Collectors.joining(", "));
+    }
+
     public static CoordinatePrinter providePrinter(final String text) {
         if (text == null) {
             return Constants.DEFAULT_PRINTER;
         }
         if (text.isBlank()) {
-            throw new UnsupportedFormatException("Empty format type is not allowed.");
+            throw new UnsupportedFormatException("Empty format type is not allowed. Use on of %s"
+                                        .formatted(commaSeparatedLabels()));
         }
 
+       
         return Arrays.stream(values())
                 .filter(type -> type.label.equals(text.trim()))
                 .map(FormatType::getPrinter)
                 .findFirst()
-                .orElseThrow(() -> new UnsupportedFormatException("Format type '%s' is not supported.".formatted(text)));
+                .orElseThrow(() -> new UnsupportedFormatException("Format type '%s' is not supported. Use one of %s"
+                                        .formatted(text, commaSeparatedLabels())));
     }
 }
