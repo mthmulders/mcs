a050b52255d4cbc91ad1582af422d9063daf3da5
Maarten Mulders <m.th.mulders@xs4all.nl>
2026-01-16 21:13:20 +0100

    chore: Remove some commented code

--- src/main/java/it/mulders/mcs/dagger/CommandLineModule.java
+++ src/main/java/it/mulders/mcs/dagger/CommandLineModule.java
@@ -10,19 +10,12 @@ import picocli.CommandLine.IExecutionExceptionHandler;
 
 @Module
 public interface CommandLineModule {
-    //    @Provides static Cli provideCli(SearchCommandHandler searchCommandHandler) {
-    //        return new Cli();
-    //    }
-
     @Provides
     static CommandLine provideCommandLine(
             final Cli cli, final DaggerFactory factory, final CommandLine.IExecutionExceptionHandler exceptionHandler) {
         return new CommandLine(cli, factory).setExecutionExceptionHandler(exceptionHandler);
     }
 
-    //    @Binds Cli.SearchCommand bindSearchCommand(final Cli.SearchCommand command);
-    //    @Binds Cli.ClassSearchCommand bindClassSearchCommand(final Cli.ClassSearchCommand command);
-    //    @Binds SearchCommandHandler bindSearchCommandHandler(final SearchCommandHandler handler);
     @Binds
     IExecutionExceptionHandler bindExecutionExceptionHandler(final McsExecutionExceptionHandler handler);
 }
